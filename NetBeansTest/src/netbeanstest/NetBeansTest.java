e167cf1768ee23ec1e19737d82e89bda3b7de2ce
MARCELO ANTONIO SOARES CARVALHO <8160287@estg.ipp.pt>
2020-03-02 01:40:04 +0000

    added netbeans test java program

---
+++ NetBeansTest/src/netbeanstest/NetBeansTest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package netbeanstest;
+
+/**
+ *
+ * @author marce
+ */
+public class NetBeansTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("It's Britney bitch");
+    }
+    
+}
